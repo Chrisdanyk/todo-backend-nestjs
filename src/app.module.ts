4d43ed6d9fbe209f932e884ef110aa05c18dc01c
chris dany <chrisdany9@gmail.com>
2025-05-26 11:30:44 +0100

    Add throttling functionality by integrating @nestjs/throttler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { TodoModule } from './modules/todo/todo.module';
 import jwtConfig from './config/jwt.config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,10 +16,25 @@ import jwtConfig from './config/jwt.config';
       load: [jwtConfig],
       envFilePath: '.env',
     }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
     AuthenticationModule,
     TodoModule,
   ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule { }
